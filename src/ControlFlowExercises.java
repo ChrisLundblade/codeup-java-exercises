8ee46e8d13828a74596b9a029661c3d40b2fd696
Christopher Lundblade <chris.lundblade@gmail.com>
2021-09-07 12:08:41 -0500

    commit for part 2 of ControlFlowExercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,56 +4,56 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         //1
-        int i = 5;
-        while (i <= 15){
-            System.out.print(i++ +" ");
-        }
-        System.out.println();
+//        int i = 5;
+//        while (i <= 15){
+//            System.out.print(i++ +" ");
+//        }
+//        System.out.println();
 
         //1b
-        i = 0;
-        do{
-            System.out.println(i+=2);
-        } while(i < 100);
-
-        i = 100;
-        do{
-            System.out.println(i-=5);
-        } while(i > -10);
-
-        i = 2;
-        do{
-            System.out.println(i);
-            i = (int) Math.pow(i,2);
-        } while(i < 1000000);
-
-        //1c
-        for (i= 5; i <= 15; i++){
-            System.out.print(i + " ");
-        }
-        System.out.println();
-
-        for (i = 0; i <= 100; i +=2){
-            System.out.println(i);
-        }
-        for (i = 100; i >= -10; i-=5){
-            System.out.println(i);
-        }
-        for (i = 2; i < 1000000; i = (int) Math.pow(i,2)){
-            System.out.println(i);
-        }
-        //2
-//        for (int i = 1; i <= 100; i++){
-//            if (i%3==0 && i%5==0){
-//                System.out.println("FizzBuzz");
-//            } else if(i%3==0){
-//                System.out.println("Fizz");
-//            } else if(i%5==0){
-//                System.out.println("Buzz");
-//            } else {
-//                System.out.println(i);
-//            }
+//        i = 0;
+//        do{
+//            System.out.println(i+=2);
+//        } while(i < 100);
+//
+//        i = 100;
+//        do{
+//            System.out.println(i-=5);
+//        } while(i > -10);
+//
+//        i = 2;
+//        do{
+//            System.out.println(i);
+//            i = (int) Math.pow(i,2);
+//        } while(i < 1000000);
+//
+//        //1c
+//        for (i= 5; i <= 15; i++){
+//            System.out.print(i + " ");
 //        }
+//        System.out.println();
+//
+//        for (i = 0; i <= 100; i +=2){
+//            System.out.println(i);
+//        }
+//        for (i = 100; i >= -10; i-=5){
+//            System.out.println(i);
+//        }
+//        for (i = 2; i < 1000000; i = (int) Math.pow(i,2)){
+//            System.out.println(i);
+//        }
+        //2
+        for (int i = 1; i <= 100; i++){
+            if (i%3==0 && i%5==0){
+                System.out.println("FizzBuzz");
+            } else if(i%3==0){
+                System.out.println("Fizz");
+            } else if(i%5==0){
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
         //3
 //        System.out.println("What number would you like to go up to?");
 //        int userInputNumber = Integer.parseInt(scanner.nextLine());
