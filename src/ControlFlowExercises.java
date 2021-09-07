570c7e4a7ca6488e568e927f932d83ae6a7cdf92
Christopher Lundblade <chris.lundblade@gmail.com>
2021-09-07 11:25:49 -0500

    i am an idiot and didnt realize i had ControlFlowExercises spelled wrong. Hopefully my commits are still okay

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ControlFloxExercises {
+public class ControlFlowExercises {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
