68c4a7bd81fe4342e9637724dd68c463ee7670de
Christopher Lundblade <chris.lundblade@gmail.com>
2021-09-09 15:58:18 -0500

    completed MethodsExercises.java

--- src/MethodWarmup.java
+++ src/MethodWarmup.java
@@ -8,15 +8,35 @@ public class MethodWarmup {
         System.out.println(getDifference(-4.16,27));
         System.out.println(getDifference(21,67));
 
+        System.out.println(addNums(3));
     }
+
+    // TODO: use recursion to add all numbers up, from 1 to the given number input
+
+    /*
+        addNums(5) would trigger a calculation of...
+            5 + 4 + 3 + 3 + 1, evaluating to 15
+     */
+     public static int addNums(int num) {
+         return addNums(num, 1, 0);
+     }
+     public static int addNums(int num, int i, int accumulator){
+         if (i == num){
+             return accumulator + i;
+         }
+         accumulator += i;
+         return addNums(num, ++i,accumulator);
+     }
 
     public static int getDifference(int num1, int num2){
         return Math.abs(num1 - num2);
     }
+
     public static int getDifference(int num){
         //we can get into the semantics of whether the difference btwn -6 and 0 is 6 or -6, but i'm saying it's 6. Fight me.
         return Math.abs(num);
     }
+
     public static double getDifference(double num1, double num2){
         return Math.abs(num1 - num2);
     }
--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
+    public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println(add(1.3,3.6));
 
@@ -7,6 +10,18 @@ public class MethodsExercises {
         System.out.println(multiply(2.1, 4.3));
 
         System.out.println(divide(4.2,2.1));
+
+        System.out.println(modulus(10,4));
+
+
+        int userInput = getInteger(1, 10);
+        System.out.println(userInput);
+
+        System.out.println(Long.MAX_VALUE);
+
+        getFactorial(getInteger(1, 100));
+
+        diceRoller();
     }
     public static double add(double num1, double num2){
         return (num1+num2);
@@ -19,5 +34,73 @@ public class MethodsExercises {
     }
     public static double divide(double num1, double num2){
         return num1/num2;
+    }
+    public static int modulus(int num1, int num2){
+        return Math.max(num1,num2) % Math.min(num1,num2);
+    }
+
+    //2
+    public static int getInteger(int min, int max){
+        System.out.print("Enter a number between" + min + " and " + max + " : ");
+        int userInput = Integer.parseInt(sc.nextLine());
+        if (userInput <= max && userInput >= min){
+            return userInput;
+        }
+        System.out.println("Your input was not in range.");
+        return getInteger(min, max);
+    }
+    //3
+    public static void getFactorial(long num){
+        System.out.printf("Do you want to continue? (Y/N)");
+        String userResponse = sc.nextLine();
+        if(userResponse.equals("N")){
+            return;
+        }
+
+        System.out.print(num + "! = " + 1 + " x ");
+        long factorial = 1;
+        for (int i = 1; i <= num; i++){
+            if(i == num){
+                factorial *= i;
+                System.out.print(" = " + factorial + "\n");
+                return;
+            }
+            if (i == num -1){
+                factorial *= i;
+                System.out.printf("%d ", i+1);
+            } else {
+                factorial *= i;
+                System.out.printf("%d x ", i + 1);
+            }
+
+        }
+
+//        if (num ==2 ){
+//            return num;
+//        }
+//
+//        return getFactorial(num-1) * num;
+        /*          writing this out for my understanding
+                                    return * 4
+                                return 3
+                             return 2 then it goes back up
+                             2*3 returns to the first call
+                             6*4 returns
+        */
+    }
+
+    //4
+    public static void diceRoller(){
+        System.out.println("How many sides shall the die we're rolling have?");
+        int numberOfSides = Integer.parseInt(sc.nextLine());
+        System.out.println("Die 1: " + (1 + Math.floor(Math.random() * (numberOfSides))) );
+        System.out.println("Die 2: " + (1 + Math.floor(Math.random() * (numberOfSides))) );
+        System.out.println("Would you like to roll again? (Y/N)");
+        String userResponse = sc.nextLine();
+        if(userResponse.contains("N") || userResponse.contains("n")){
+            return;
+        } else if(userResponse.contains("Y") || userResponse.contains("y")){
+            diceRoller();
+        }
     }
 }
