e264e9ec91e90cf81a5b8d8a63ddf11455f49dd1
Christopher Lundblade <chris.lundblade@gmail.com>
2021-09-10 20:21:00 -0500

    completed base version (not bonus) of Circle.java and CircleApp.java

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,4 +1,22 @@
 package shapes;
+import util.Input;
 
 public class CircleApp {
+    public static void main(String[] args) {
+        Input input = new Input();
+        System.out.println("What is the radius of the circle in meters?");
+        double radius = input.getDouble(0, (double) Integer.MAX_VALUE);
+        Circle myCircle = new Circle(radius);
+        System.out.println("My circle has an area of " + myCircle.getArea() + " square meters.");
+        System.out.println("My circle has a circumference of " + myCircle.getCircumference() + " meters.");
+
+//        System.out.println("Keep making circles?");
+//        boolean keepMakingCircles = input.yesNo();
+//        while (keepMakingCircles){
+//
+//
+//            System.out.println("Keep making circles?");
+//            keepMakingCircles= input.yesNo();
+//        }
+    }
 }
