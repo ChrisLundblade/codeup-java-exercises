aea968c51db99565d4bab62927ecf9487d039035
Christopher Lundblade <chris.lundblade@gmail.com>
2021-10-21 09:27:21 -0500

    completed flipInnerCase method in JavaDrills.java

--- src/JavaDrills.java
+++ src/JavaDrills.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.Locale;
 import java.util.List;
@@ -32,16 +33,34 @@ public class JavaDrills {
         }
         return sum1-sum2;
     }
+    public static String flipInnerCase(String str){
+        String flippedCaseString = str.substring(0,1);
+        for(int i =1; i < str.length()-1; i++){
+            if(str.substring(i,i+1).toUpperCase().equals(str.substring(i,i+1))){
+                flippedCaseString += str.substring(i,i+1).toLowerCase();
+            }else{
+                flippedCaseString += str.substring(i,i+1).toUpperCase();
+            }
+        }
+        flippedCaseString += str.substring(str.length()-1,str.length());
+        return flippedCaseString;
+    }
 
 
     public static void main(String[] args) {
-        System.out.println(flipOuterCase("cat")); // CaT
-        System.out.println(flipOuterCase("CaT")); // cat
-        System.out.println(flipOuterCase("caT")); // Cat
-        System.out.println(flipOuterCase("cAt")); // CAT
+//        System.out.println(flipOuterCase("cat")); // CaT
+//        System.out.println(flipOuterCase("CaT")); // cat
+//        System.out.println(flipOuterCase("caT")); // Cat
+//        System.out.println(flipOuterCase("cAt")); // CAT
+//
+//        System.out.println(returnTotalDifference(Arrays.asList(10, 2, 3), Arrays.asList(1, 2, 3)));
+//        System.out.println(returnTotalDifference(Arrays.asList(10, 1), Arrays.asList(1, 7)));
+//        System.out.println(returnTotalDifference(Arrays.asList(10, 1), Arrays.asList(1, 7, 1)));
 
-        System.out.println(returnTotalDifference(Arrays.asList(10, 2, 3), Arrays.asList(1, 2, 3)));
-        System.out.println(returnTotalDifference(Arrays.asList(10, 1), Arrays.asList(1, 7)));
-        System.out.println(returnTotalDifference(Arrays.asList(10, 1), Arrays.asList(1, 7, 1)));
+        System.out.println(flipInnerCase("cat")); // cAt
+        System.out.println(flipInnerCase("CaT")); // CAT
+        System.out.println(flipInnerCase("caT")); // cAT
+        System.out.println(flipInnerCase("codeup")); // cODEUp
+        System.out.println(flipInnerCase("CoDeup")); // COdEUp
     }
 }
